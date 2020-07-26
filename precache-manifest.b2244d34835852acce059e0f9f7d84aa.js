abe0419ad72708c673271074f4537417833dbc7e
RayanAbid <rayanabid12345@gmail.com>
2020-07-26 23:15:01 +0500

    Updates

---
+++ precache-manifest.b2244d34835852acce059e0f9f7d84aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b582a0ffbe197638aabbd0cd2cb16691",
+    "url": "/Covid-19-Tracker/index.html"
+  },
+  {
+    "revision": "d4da468476bf45a5b2af",
+    "url": "/Covid-19-Tracker/static/css/main.61c658ee.chunk.css"
+  },
+  {
+    "revision": "a16cbdc951eebdcdcaea",
+    "url": "/Covid-19-Tracker/static/js/2.b84e73b4.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/Covid-19-Tracker/static/js/2.b84e73b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4da468476bf45a5b2af",
+    "url": "/Covid-19-Tracker/static/js/main.577a9aca.chunk.js"
+  },
+  {
+    "revision": "3f9bfa923e184b907d1a",
+    "url": "/Covid-19-Tracker/static/js/runtime-main.f79461fb.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid-19-Tracker/static/media/image.d7265326.png"
+  }
+]);
